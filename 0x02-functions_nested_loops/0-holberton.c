60943a9a8fbae99c9f3dac3dd3f6a67335be487b
Ruben Cadenas <ruben.cadenas@hotmail.com>
2021-06-24 09:29:47 -0700

    Adding header file for the task 0. _putchar

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+int main(void)
+{
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar(10); /* _putchat('\n');  */
+
+	return (0);
+}
